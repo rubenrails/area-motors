edfeea55757deda62b0e39c656c75309ee252fb9
Ruben Ascencio <hi@rubenascenc.io>
2018-08-07 16:44:53 -0700

    Add delegation so listing attrs are easier to get

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -1,4 +1,6 @@
 class Enquiry < ApplicationRecord
   belongs_to :source, class_name: 'Website'
   has_one :car_listing
+
+  delegate :make, :model, :colour, :year, :reference, :url, to: :car_listing
 end
