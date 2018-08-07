789225684871b2867251f8ada6737c2c17473da2
Ruben Ascencio <hi@rubenascenc.io>
2018-08-07 16:52:11 -0700

    Accept nested attributes for car listing

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -2,5 +2,7 @@ class Enquiry < ApplicationRecord
   belongs_to :source, class_name: 'Website'
   has_one :car_listing
 
+  accepts_nested_attributes_for :car_listing, reject_if: :all_blank
+
   delegate :make, :model, :colour, :year, :reference, :url, to: :car_listing
 end
