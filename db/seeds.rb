31f6a615576e8e3dfc85506ce0fb90fde70fb6bf
Ruben Ascencio <hi@rubenascenc.io>
2018-08-07 16:42:00 -0700

    Add websites seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+websites = Website.create([{ name: 'AMDirect', domain: 'amdirect.test'}, { name: 'CarsForSale', domain: 'carsforsale.test'}])
