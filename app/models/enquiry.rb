37e205eb1885ff401bf252632b01417ed3254d60
Ruben Ascencio <hi@rubenascenc.io>
2018-08-07 19:40:20 -0700

    Add status column (enum) to Enquiry

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -5,4 +5,6 @@ class Enquiry < ApplicationRecord
   accepts_nested_attributes_for :car_listing, reject_if: :all_blank
 
   delegate :make, :model, :colour, :year, :reference, :url, to: :car_listing
+
+  enum status: [:new, :invalid, :done, :expired, :junk], _prefix: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_233146) do
+ActiveRecord::Schema.define(version: 2018_08_08_021037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_08_07_233146) do
     t.bigint "source_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["source_id"], name: "index_enquiries_on_source_id"
   end
 
