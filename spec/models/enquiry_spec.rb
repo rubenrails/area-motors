7034b5231f34bba4f54385098a60902a89e43e59
Ruben Ascencio <hi@rubenascenc.io>
2018-08-07 20:51:13 -0700

    Add a bunch of state machine tests

--- spec/models/enquiry_spec.rb
+++ spec/models/enquiry_spec.rb
@@ -15,4 +15,87 @@ RSpec.describe Enquiry, type: :model do
     it { is_expected.to delegate_method(:url).to(:car_listing) }
   end
 
+  describe '#status' do
+    subject { described_class.new }
+
+    describe '#invalidate' do
+      it { is_expected.to transition_from(:new).to(:invalid).on_event(:invalidate) }
+    end
+
+    describe '#mark_as_done' do
+      it { is_expected.to transition_from(:new).to(:done).on_event(:mark_as_done) }
+    end
+
+    describe '#expire' do
+      it { is_expected.to transition_from(:new).to(:expired).on_event(:expire) }
+    end
+
+    describe '#mark_as_junk' do
+      it { is_expected.to transition_from(:new).to(:junk).on_event(:mark_as_junk) }
+      it { is_expected.to transition_from(:invalid).to(:junk).on_event(:mark_as_junk) }
+    end
+
+    describe '#reset' do
+      it { is_expected.to transition_from(:invalid).to(:new).on_event(:reset) }
+      it { is_expected.to transition_from(:done).to(:new).on_event(:reset) }
+      it { is_expected.to transition_from(:expired).to(:new).on_event(:reset) }
+      it { is_expected.to transition_from(:junk).to(:new).on_event(:reset) }
+    end
+
+    context 'when it is in initial NEW state' do
+      subject { described_class.new }
+
+      it { is_expected.to have_state(:new) }
+      it { is_expected.to allow_event(:invalidate) }
+      it { is_expected.to allow_event(:mark_as_done) }
+      it { is_expected.to allow_event(:expire) }
+      it { is_expected.to allow_event(:mark_as_junk) }
+      it { is_expected.to_not allow_event(:reset) }
+    end
+
+    context 'when it is in INVALID state' do
+      subject { described_class.new status: :invalid }
+
+      it { is_expected.to have_state(:invalid) }
+      it { is_expected.to allow_event(:mark_as_junk) }
+      it { is_expected.to allow_event(:reset) }
+      it { is_expected.to_not allow_event(:invalidate) }
+      it { is_expected.to_not allow_event(:mark_as_done) }
+      it { is_expected.to_not allow_event(:expire) }
+    end
+
+    context 'when it is in DONE state' do
+      subject { described_class.new status: :done }
+
+      it { is_expected.to have_state(:done) }
+      it { is_expected.to allow_event(:reset) }
+      it { is_expected.to_not allow_event(:invalidate) }
+      it { is_expected.to_not allow_event(:mark_as_done) }
+      it { is_expected.to_not allow_event(:expire) }
+      it { is_expected.to_not allow_event(:mark_as_junk) }
+    end
+
+    context 'when it is in EXPIRED state' do
+      subject { described_class.new status: :expired }
+
+      it { is_expected.to have_state(:expired) }
+      it { is_expected.to allow_event(:reset) }
+      it { is_expected.to_not allow_event(:invalidate) }
+      it { is_expected.to_not allow_event(:mark_as_done) }
+      it { is_expected.to_not allow_event(:expire) }
+      it { is_expected.to_not allow_event(:mark_as_junk) }
+    end
+
+    context 'when it is in JUNK state' do
+      subject { described_class.new status: :junk }
+
+      it { is_expected.to have_state(:junk) }
+      it { is_expected.to allow_event(:reset) }
+      it { is_expected.to_not allow_event(:invalidate) }
+      it { is_expected.to_not allow_event(:mark_as_done) }
+      it { is_expected.to_not allow_event(:expire) }
+      it { is_expected.to_not allow_event(:mark_as_junk) }
+    end
+  end
+
 end
